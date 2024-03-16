f49ede3f177aa6110ada9816cc742173f3bcdf3e
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2024-03-16 16:45:36 +0800

    Initial migration

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
-mod handler;
-mod routes;
+use proto::vault_server::{Vault, VaultServer};
+use tonic::transport::Server;
+
+mod proto {
+    tonic::include_proto!("vault");
+}
+
+#[derive(Debug, Default)]
+struct VaultService {}
+
+#[tonic::async_trait]
+impl Vault for VaultService {
+    async fn tokenize(
+        &self,
+        request: tonic::Request<proto::TokenizeRequest>,
+    ) -> Result<tonic::Response<proto::TokenizeResponse>, tonic::Status> {
+        let req = request.into_inner();
+
+        let resp = proto::TokenizeResponse {
+            tokenized_value: req.value,
+            tokenized_field: req.field,
+        };
+
+        Ok(tonic::Response::new(resp))
+    }
+}
 
 #[tokio::main]
-async fn main() {
-    let app = routes::create_api_route(); // Create the service, with the handler
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let vault = VaultService::default();
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap(); // Create a listener
-
-    axum::serve(listener, app).await.unwrap(); // Serve the service through the listener
+    Server::builder()
+        .add_service(VaultServer::new(vault))
+        .serve(addr)
+        .await?;
+    println!("Listening on {addr}");
+    Ok(())
 }
--- src/routes.rs
+++
@@ -1,6 +0,0 @@
-use crate::handler;
-use axum::{routing::get, Router};
-pub fn create_api_route() -> Router {
-    let router = Router::new().route("/decrypt", get(handler::decrypt));
-    router
-}
